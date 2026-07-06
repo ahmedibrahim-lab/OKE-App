1da94a876c52a76adc482e5a31588d7dc79d5be8
Ahmed Ibrahim <ahmedibrahim@Mac.mynet>
2026-07-06 21:31:12 +0100

    add basic playwright test

---
+++ tests/basic.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://143.47.254.99/#pod-info');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/OKE Cloud-Native App/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('http://143.47.254.99/#pod-info');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Explore Pipeline' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('span', { name: 'CI/CD DevSecOps & GitOps Pipeline' })).toBeVisible();
+});
